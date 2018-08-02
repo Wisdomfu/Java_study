6fe0194a0f8f95ec5af7af7de5b70490f70c107f
Wisdomfu <1173229585@qq.com>
2018-08-02 12:43:51 +0800

    新增二维数组的练习题——找随机二维[5][5]数组的最大值并输出坐标

--- 二维数组.java
+++ 二维数组.java
@@ -17,6 +17,48 @@ public class 二维数组 {
 		};
 		c[2][1]=0;
 		System.out.println(c[2][0]+c[2][1]+c[2][2]);
+		
+		//练习：定义一个5X5的二维数组。 然后使用随机数填充该二维数组。
+		//找出这个二维数组里，最大的那个值，并打印出其二维坐标
+		int[][] d=new int[5][5];
+		//System.out.println(d.length);
+		for(int j=0;j<d.length;j++) {
+			for(int k=0;k<d[0].length;k++) {
+				d[j][k]=randomnum();
+			}
+		}
+		show(d);
+		findmax(d);
 	}
-
+	
+	public static int randomnum() {//获取随机数
+		return (int)(Math.random()*100);
+	}
+	
+	public static void show(int d[][]) {//显示二维数组内容
+		System.out.println("show:");
+		for(int j=0;j<d.length;j++) {
+			for(int k=0;k<d[0].length;k++) {
+				System.out.print(" "+d[j][k]);
+			}
+			System.out.println();
+		}
+		System.out.println("\n");
+	}
+	
+	public static void findmax(int d[][]) {//找到并输出最大值，且输出坐标值
+		int n=0;
+		int x=0,y=0;
+		for(int j=0;j<d.length;j++) {
+			for(int k=0;k<d[0].length;k++) {
+				if(d[j][k]>n) {
+					n=d[j][k];
+					x=j;
+					y=k;
+				}
+			}
+		}
+		System.out.println("最大值为："+n);
+		System.out.println("坐标  x:"+x+"  y:"+y);
+	}
 }
