3b4f5858cd816b0bab63ecd6026f59bc62fc48c9
Wisdomfu <1173229585@qq.com>
2018-08-30 16:04:00 +0800

    commit 格式化输出.java

---
+++ 格式化输出.java
@@ -0,0 +1,35 @@
+import java.util.Locale;
+public class 格式化输出 {
+	public static void main(String[] args) {
+		String name="亚索";
+		int kill=8;
+		String title="超神";
+		
+		//格式化输出
+		String a="%s在进行了连续%d次击杀后，获得了%s的称号%n";
+		System.out.printf(a,name,kill,title);
+		
+		
+		int year = 2020;
+        //总长度，左对齐，补0，千位分隔符，小数点位数，本地化表达
+          
+        //直接打印数字
+        System.out.format("%d%n",year);
+        //总长度是8,默认右对齐
+        System.out.format("%8d%n",year);
+        //总长度是8,左对齐
+        System.out.format("%-8d%n",year);
+        //总长度是8,不够补0
+        System.out.format("%08d%n",year);
+        //千位分隔符
+        System.out.format("%,8d%n",year*10000);
+  
+        //小数点位数
+        System.out.format("%.2f%n",Math.PI);
+          
+        //不同国家的千位分隔符
+        System.out.format(Locale.FRANCE,"%,.2f%n",Math.PI*10000);
+        System.out.format(Locale.US,"%,.2f%n",Math.PI*10000);
+        System.out.format(Locale.UK,"%,.2f%n",Math.PI*10000);
+	}
+}
