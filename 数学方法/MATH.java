ff8e3f551ee23cbe46cf2853dcb7cf61ab940b68
Wisdomfu <1173229585@qq.com>
2018-08-30 15:17:19 +0800

    commit MATH.java

--- 数学方法/MATH.java
+++ 数学方法/MATH.java
@@ -1,11 +1,28 @@
 package 数学方法;
 
 public class MATH {
-	public static double e() {
+	public static double e() {//练习方法，求自然对数e的值
 		double y=0;//long i=Long.MAX_VALUE;
 		double x=9.223372036854776E14;
 		y=Math.pow(1+(1/x),x);
 		return y;
+	}
+	
+	//练习方法，计算质数个数
+	public static String countPrime() {
+		return"本函数计算0到上限值中的质数个数，请传入上限参数！";
+	}
+	
+	public static int countPrime(int num) {
+		int count=0;
+		for(int i=1;i<=num;i++) {//外层循环，1~num
+			int count1=0;
+			for(int j=1;j<=i;j++) {//内层，计算公因数个数（判断是否为质数）
+				if(i%j==0) count1++;
+			}
+			if(count1==2) count++;
+		}
+		return count;
 	}
 	
 	public static void main(String[] args) {
@@ -33,5 +50,6 @@ public class MATH {
         System.out.println(Math.E);
 
         System.out.println(e());
+        System.out.println(countPrime(10000000));
 	}
 }
